51df019de595439be8811f3d70b55bc3fb70cf75
Rajwansh Bhati <rajwansh.bhati@calsoftinc.com>
2025-06-12 18:38:20 +0530

    Done2

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-import express from "express";
-import mongoose from "mongoose";
-import userModel from "./user.model.js";
-import cors from "cors"
+const userModel = require("./user.model");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-mongoose.connect("mongodb://mongo:27017/db")
+mongoose
+  .connect("mongodb://mongo:27017/db")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
