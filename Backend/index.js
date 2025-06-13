ea49e3fd52b93819aa45617f9853fc1b021387aa
Rajwansh Bhati <rajwansh.bhati@calsoftinc.com>
2025-06-13 10:40:40 +0530

    last2

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import express from "express"
 import mongoose from "mongoose";
 import cors   from "cors"
 
-import userModel from "./user.model"
+import userModel from "./user.model.js"
 
 const app = express();
 const PORT = 3000;
